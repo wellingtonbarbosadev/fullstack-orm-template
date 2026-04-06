4c133f26a35ce6718f3386d8f72d6d1f437db8d3
wellingtonbarbosadev <106507628+wellingtonbarbosadev@users.noreply.github.com>
2026-04-06 15:23:18 -0300

    feat: implement filtering and sorting in index method of QuestionsController

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -3,7 +3,17 @@ import { prisma } from "@/prisma"
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    const questions = await prisma.question.findMany()
+    const questions = await prisma.question.findMany({
+      where: {
+        title: { 
+          contains: request.query.title?.toString().trim(),
+          mode: "insensitive"
+        },
+      },
+      orderBy: {
+        title: "asc"
+      }
+    })
 
     return response.json(questions)
   }
@@ -16,10 +26,27 @@ class QuestionsController {
   }
 
   async update(request: Request, response: Response) {
+    const { title, content } = request.body
+    const { id } = request.params
+
+    await prisma.question.update({ 
+      where: {
+        id
+      }, 
+      data: {
+        title,
+        content
+      }
+    })
+
     return response.json()
   }
 
   async remove(request: Request, response: Response) {
+    const { id } = request.params
+
+    await prisma.question.delete({ where: { id } })
+    
     return response.json()
   }
 }
